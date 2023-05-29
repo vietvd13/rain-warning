091de2f679eab09cdba6d2f3ebac0c8893961163
Duck <vuducviet0131@gmail.com>
2023-05-29 22:44:00 +0700

    Update log

--- sendNoti/sendMail.js
+++ sendNoti/sendMail.js
@@ -2,6 +2,35 @@ const nodemailer =  require('nodemailer');
 const { templateMailRainWarning } = require('../mail/templateMailRainWarning');
 const { templateMailSummerWarning } = require('../mail/templateMailSummerWarning');
 const { templateMailGoodNight } = require('../mail/templateMailGoodNight');
+const { templateMailQuote } = require('../mail/templateMailQuote');
+
+async function mailLogError(type, error) {
+    console.log("[LOG] - SEND MAIL LOG ERROR...");
+
+    const transporter = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        secure: false,
+        auth: {
+            user: process.env.SMTP_EMAIL,
+            pass: process.env.SMTP_PASSWORD
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+
+    transporter.verify().then(console.log).catch(console.error);
+
+    const mailOptions = {
+        from: `${process.env.FROM_NAME} <${process.env.SMTP_EMAIL}>`,
+        to: "vuducviet0131@gmail.com",
+        subject: `[Duck in Love] - ERROR: ${type}`,
+        html: `<span>Log send mail: ${error}</span>`
+    };
+
+    await transporter.sendMail(mailOptions);
+}
 
 async function sendMailRainWarning() {
     console.log("[LOG] - SEND MAIL WARNING RAIN...");
@@ -32,6 +61,8 @@ async function sendMailRainWarning() {
 }
 
 async function sendMailSummerWarning() {
+    console.log("[LOG] - SEND MAIL SUMMER WARNING...");
+
     const transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
         port: process.env.SMTP_PORT,
@@ -58,6 +89,8 @@ async function sendMailSummerWarning() {
 }
 
 async function sendMailGoodNight() {
+    console.log("[LOG] - SEND MAIL GOOD NIGHT...");
+
     const transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
         port: process.env.SMTP_PORT,
@@ -101,13 +134,16 @@ async function sendMailQuote() {
         from: `${process.env.FROM_NAME} <${process.env.SMTP_EMAIL}>`,
         to: process.env.TO_EMAIL,
         subject: "Nhắn gửi yêu thương by Vũ Duck",
-        html: templateMailWeather(data)
+        html: templateMailQuote()
     };
+
     await transporter.sendMail(mailOptions);
 }
 
 module.exports = {
+    mailLogError,
     sendMailRainWarning,
     sendMailSummerWarning,
-    sendMailGoodNight
+    sendMailGoodNight,
+    sendMailQuote
 }
